81fe571434b0df448d0e872a1d0d42306a995228
Cathal Sweeney <cathalsweeney96@hotmail.com>
2024-09-24 11:37:53 -0500

    will add some info to plots on fraction of events with various delta_t

--- lappd/pps/plot_missing.C
+++ lappd/pps/plot_missing.C
@@ -9,7 +9,9 @@
 #include "TCanvas.h"
 #include <iostream>
 
-void PlotAll(std::vector<std::pair<TH1F*, TH1F*>> vHist)
+int nLAPPD = 3;
+
+void PlotAll(std::vector<std::pair<TH1F*, TH1F*>> vHist, std::string outname)
 {
 
   TCanvas* c = new TCanvas("", "", 1300,600);
@@ -27,11 +29,21 @@ void PlotAll(std::vector<std::pair<TH1F*, TH1F*>> vHist)
 
   // Divide the canvas into three vertical segments
   c->Divide(3, 2);  // 3 columns, 2 rows
-  
+
+  double textSize = 0.06;
   // Plot in each segment
   c->cd(1);
   gPad->SetLogy(true);
   vHist[0].first->Draw(); 
+  TLatex* text1 = new TLatex(-3e8,800,"foo");
+  text1->SetTextSize(textSize);
+  text1->Draw("SAME");
+  TLatex* text2 = new TLatex(-3e8,200,"bar");
+  text2->SetTextSize(textSize);
+  text2->Draw("SAME");
+  TLatex* text3 = new TLatex(-3e8,50,"bar");
+  text3->SetTextSize(textSize);
+  text3->Draw("SAME");
   c->cd(4);
   gPad->SetLogy(true);
   vHist[0].second->Draw();
@@ -49,12 +61,16 @@ void PlotAll(std::vector<std::pair<TH1F*, TH1F*>> vHist)
   c->cd(6);
   gPad->SetLogy(true);
   vHist[2].second->Draw(); 
- 
-  
+   
   // Update the canvas
   c->Update();
 
-  c->SaveAs("all.png");
+  c->SaveAs(outname.c_str() );
+  
+  for(int i=0; i<3; i++){
+    std::cout << vHist[i].first->Integral(0,101) << "\n";
+    std::cout << vHist[i].second->Integral(0,101) << "\n";
+  }
 }
 
 void plot_missing()
@@ -62,6 +78,7 @@ void plot_missing()
   
   //TFile *inFile = new TFile("~/mount/from_yue/ToolAnalysis/processed/4765/LAPPDTree.root");
   TFile *inFile = new TFile("~/mount/from_yue/ToolAnalysis/processed/5007/LAPPDTree.root");
+  //TFile *inFile = new TFile("~/mount/from_yue/ToolAnalysis/processed/5008/LAPPDTree.root");
 
   TTree *myTree;
   myTree = (TTree*)inFile->Get("TimeStamp");
@@ -72,13 +89,6 @@ void plot_missing()
   ULong64_t ppsTime1;
   int LAPPD_ID;
   
-  Long64_t xLo = -4e8;
-  Long64_t xHi = 7e8;
-  int nBins = 100;
-  
-//  Long64_t xLo = 320000000 - 15;
-//  Long64_t xHi = 320000000 + 15;
-//  int nBins = 30;
   
   myTree->SetBranchAddress("ppsTime0",&ppsTime0);
   myTree->SetBranchAddress("ppsTime1",&ppsTime1);
@@ -86,32 +96,54 @@ void plot_missing()
 
   std::string xLabel = "#Delta t_{pps} (clock ticks)";
   std::string yLabel = "Events";
+  Long64_t xLo = -4e8;
+  Long64_t xHi = 7e8;
+  int nBins = 100;
   double yLo = 5e-1;
   double yHi = 1e6;
+
+
+  std::string xLabel_zoom = "#Delta t_{pps} - 3.2e8 (clock ticks)";
+  Long64_t xLo_zoom = -15;
+  Long64_t xHi_zoom = 15;
+  int nBins_zoom = xHi_zoom - xLo_zoom;
+  double yLo_zoom = 5e-1;
+  double yHi_zoom = 1e6;
   
   std::vector<std::pair<TH1F*,TH1F*>> hDiff_vec; // pair of hists {acdc0, acdc1} for each LAPPD ID
-  int nLAPPD = 3;
+  std::vector<std::pair<TH1F*,TH1F*>> hDiff_vec_zoom; // same thing, but we subtract 3.2e8 (= 1s) from entries
   for(int i=0; i<nLAPPD; i++){
     TH1F *hACDC0  = new TH1F("", "", nBins, xLo, xHi);
     hACDC0->GetXaxis()->CenterTitle(true);
     hACDC0->GetYaxis()->CenterTitle(true);
     hACDC0->GetXaxis()->SetTitle(xLabel.c_str());
     hACDC0->GetYaxis()->SetTitle(yLabel.c_str());
-    hACDC0->GetYaxis()->SetRangeUser(yLo, yHi);
+    hACDC0->GetYaxis()->SetRangeUser(yLo, yHi);    
     hACDC0->SetTitle("ACDC 0");
-
-    TH1F *hACDC1  = new TH1F("", "", nBins, xLo, xHi);
-    hACDC1->GetXaxis()->CenterTitle(true);
-    hACDC1->GetYaxis()->CenterTitle(true);
-    hACDC1->GetXaxis()->SetTitle(xLabel.c_str());
-    hACDC1->GetYaxis()->SetTitle(yLabel.c_str());
-    hACDC1->GetYaxis()->SetRangeUser(yLo, yHi);
-    hACDC1->SetTitle("ACDC 1");
-
-
+    TH1F* hACDC1 = (TH1F*) hACDC0->Clone();
+    hACDC1->SetTitle("ACDC 1");    
     hDiff_vec.push_back({hACDC0, hACDC1});
+
+    TH1F *hACDC0_zoom  = new TH1F("", "", nBins_zoom, xLo_zoom, xHi_zoom);
+    hACDC0_zoom->GetXaxis()->CenterTitle(true);
+    hACDC0_zoom->GetYaxis()->CenterTitle(true);
+    hACDC0_zoom->GetXaxis()->SetTitle(xLabel_zoom.c_str());
+    hACDC0_zoom->GetYaxis()->SetTitle(yLabel.c_str());
+    hACDC0_zoom->GetYaxis()->SetRangeUser(yLo_zoom, yHi_zoom);    
+    hACDC0_zoom->SetTitle("ACDC 0");
+    TH1F* hACDC1_zoom = (TH1F*) hACDC0_zoom->Clone();
+    hACDC1_zoom->SetTitle("ACDC 1");    
+    hDiff_vec_zoom.push_back({hACDC0_zoom, hACDC1_zoom});
+
+
   }
 
+
+  std::vector<std::pair<std::vector<int>,std::vector<int> > > vCounts; // count number of events where delta_t is 0, 3.2e8, or other
+  for(int i=0; i<nLAPPD; i++){
+    std::pair<std::vector<int>,std::vector<int> > pair = { {0,0,0} , {0,0,0} };
+    vCounts.push_back(pair);
+  }
   
   std::pair<Long64_t, Long64_t> pps_prev = {0, 0}; // pps timestamp from the previous event {ACDC0, ACDC1}
   std::pair<Long64_t, Long64_t> pps_current = {0, 0}; // pps timestamp from the current event {ACDC0, ACDC1}
@@ -129,7 +161,21 @@ void plot_missing()
 
     hDiff_vec[LAPPD_ID].first->Fill(diff.first);
     hDiff_vec[LAPPD_ID].second->Fill(diff.second);
-//
+
+    hDiff_vec_zoom[LAPPD_ID].first->Fill(diff.first - 3.2e8);
+    hDiff_vec_zoom[LAPPD_ID].second->Fill(diff.second - 3.2e8);
+
+    if(diff.first == 0) vCounts[LAPPD_ID].first.at(0)++;
+    else if(diff.first == 3.2e8) vCounts[LAPPD_ID].first.at(1)++;
+    else vCounts[LAPPD_ID].first.at(2)++;
+
+    if(diff.second == 0) vCounts[LAPPD_ID].second.at(0)++;
+    else if(diff.second == 3.2e8) vCounts[LAPPD_ID].second.at(1)++;
+    else vCounts[LAPPD_ID].second.at(2)++;
+
+
+    
+    //
 //    if(diff.first > xHi || diff.first < xLo){
 //      std::cout << pps_prev.first << " : " << pps_current.first << " : " << diff.first <<  "\n";
 //    }
@@ -137,37 +183,20 @@ void plot_missing()
 //      std::cout << pps_prev.second << " : " << pps_current.second << " : " << diff.second <<  "\n";
 //    }
 
-
     
   }
 
-  PlotAll(hDiff_vec);
+
+  for(int i=0; i<nLAPPD; i++){
+    for(int j : vCounts[i].first) std::cout << j << ", ";
+    std::cout << "\n";
+    for(int j : vCounts[i].second) std::cout << j << ", ";
+    std::cout << "\n";
+  }
+
   
-//  TCanvas* c = new TCanvas();
-//  c->SetLogy(true);
-//
-//  std::string xLabel = "#Delta t_{pps} (clock ticks)";
-//  std::string yLabel = "Events";
-//  double yLo = 5e-1;
-//  double yHi = 1e6;
-//  for(int i=0; i<nLAPPD; i++){
-//    hDiff_vec[i].first->GetXaxis()->CenterTitle(true);
-//    hDiff_vec[i].first->GetYaxis()->CenterTitle(true);
-//    hDiff_vec[i].first->GetXaxis()->SetTitle(xLabel.c_str());
-//    hDiff_vec[i].first->GetYaxis()->SetTitle(yLabel.c_str());
-//    hDiff_vec[i].first->GetYaxis()->SetRangeUser(yLo, yHi);
-//    hDiff_vec[i].first->SetTitle("ACDC 0");
-//    hDiff_vec[i].first->Draw("HIST");
-//    c->SaveAs( (std::to_string(i)+"_0.png").c_str() );
-//
-//    hDiff_vec[i].second->GetXaxis()->CenterTitle(true);
-//    hDiff_vec[i].second->GetYaxis()->CenterTitle(true);
-//    hDiff_vec[i].second->GetXaxis()->SetTitle(xLabel.c_str());
-//    hDiff_vec[i].second->GetYaxis()->SetTitle(yLabel.c_str());
-//    hDiff_vec[i].second->GetYaxis()->SetRangeUser(yLo, yHi);
-//    hDiff_vec[i].second->SetTitle("ACDC 1");
-//    hDiff_vec[i].second->Draw("HIST");
-//    c->SaveAs( (std::to_string(i)+"_1.png").c_str() );
-//  }
+  PlotAll(hDiff_vec, "all.png");
+  std::cout << "------------------- \n";
+  PlotAll(hDiff_vec_zoom, "all_zoom.png");
 }
 
